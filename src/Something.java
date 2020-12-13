d2916948f07ec7680cfe3cc1f226651cc6847b0d
Alexandra Rogovenko <a.rogovenko@syberry.com>
2020-12-13 15:27:46 +0300

    code change

--- src/Something.java
+++ src/Something.java
@@ -1,5 +1,7 @@
 public class Something {
     public static void main(String[] args) {
-        System.out.println("Hello participant #" + Math.random());
+        System.out.println("Hello participant number:" + Math.random());
+
+        System.out.println("p.s. from Nastya");
     }
 }
